77fcd4768e335aecca6cb31cb7540847ef4fea9f
vishal makwana <makwanavishal9898@gmail.com>
2024-01-03 19:09:15 +0530

    done

--- src/admin/container/Shop/Shop.js
+++ src/admin/container/Shop/Shop.js
@@ -1,93 +1,98 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { DataGrid } from '@mui/x-data-grid';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { DataGrid } from "@mui/x-data-grid";
+import { useState } from "react";
+import { useEffect } from "react";
 
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
-import ShopForm from './ShopForm'
-import { useDispatch, useSelector } from 'react-redux';
-import { addShopdata } from '../../../container/slice/shop.slice';
-
-
+import ShopForm from "./ShopForm";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addShopdata,
+  deleteShopdata,
+  getShopdata,
+  updateShopdata,
+} from "../../../container/slice/shop.slice";
 
 export default function Shop() {
-    const [updte, setUpdate] = useState(false)
+  const [updte, setUpdate] = useState(false);
 
-    // const shop = useSelector(state => state.shop)
-    // console.log(shop);
+  // const shop = useSelector(state => state.shop)
+  // console.log(shop);
 
-    const shop  = useSelector(state => state.shop)
-    console.log(shop);
-   
-    const dispatch = useDispatch()
-    
-    const handleFormSubmit = (data) => {
-        
-        dispatch(addShopdata(data))
+  const shop = useSelector((state) => state.shop);
+  console.log(shop);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getShopdata();
+  }, []);
+
+  const handleFormSubmit = (data) => {
+    if (updte) {
+      dispatch(updateShopdata(data));
+    } else {
+      dispatch(addShopdata(data));
     }
+  };
 
-    
-    
+  const handleEdit = (data) => {
+    setUpdate(data);
+  };
 
-    const handleEdit = () => {
-       
+  const handleDelete = (id) => {
+    console.log(id);
+    dispatch(deleteShopdata(id));
+  };
 
-       
-    }
+  const columns = [
+    { field: "name", headerName: "Name", width: 130 },
+    { field: "price", headerName: "Price", width: 130 },
+    { field: "date", headerName: "date", width: 130 },
+    {
+      field: "Action",
+      headerName: "Action",
 
-    const handleDelete = () => {
-       
+      renderCell: (params) => (
+        <>
+          <IconButton
+            aria-label="delete"
+            onClick={() => handleEdit(params.row)}
+          >
+            <EditIcon />
+          </IconButton>
 
-    }
+          <IconButton
+            aria-label="delete"
+            onClick={() => handleDelete(params.row.id)}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </>
+      ),
+    },
+  ];
 
-    const columns = [
-        { field: 'name', headerName: 'Name', width: 130 },
-        { field: 'price', headerName: 'Price', width: 130 },
-        { field: 'date', headerName: 'date', width: 130 },
-        {
-            field: 'Action', headerName: 'Action',
-
-            renderCell: (params) => (
-                <>
-                    <IconButton aria-label="delete" onClick={() => handleEdit(params.row)}>
-                        <EditIcon />
-                    </IconButton>
-
-                    <IconButton aria-label="delete" onClick={() => handleDelete(params.row.id)}>
-                        <DeleteIcon />
-                    </IconButton>
-                </>
-            )
-
-        },
-
-    ];
-
-   
-
-
-    return (
-        <div>
-            <ShopForm onHandleSubmit={handleFormSubmit} />
-            <div style={{ height: 400, width: '100%' }}>
-                <DataGrid
-                     rows={shop.shop}
-                    columns={columns}
-                    initialState={{
-                        pagination: {
-                            paginationModel: { page: 0, pageSize: 5 },
-                        },
-                    }}
-                    pageSizeOptions={[5, 10]}
-                    checkboxSelection
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <ShopForm onHandleSubmit={handleFormSubmit} onupdte={updte} />
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={shop.shop}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
+      </div>
+    </div>
+  );
 }
-
-
--- src/container/slice/shop.slice.js
+++ src/container/slice/shop.slice.js
@@ -1,50 +1,110 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import { deleteObject, getDownloadURL, getMetadata, getStorage, ref, uploadBytes } from "firebase/storage";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  deleteObject,
+  getDownloadURL,
+  getMetadata,
+  getStorage,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
 // import { db, storage } from "../firebase";
 import { Collections, DoorBack } from "@mui/icons-material";
-import { addDoc, collection } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { db, storage } from "../../firebase";
 
-
-
 const initialState = {
-    isLoding: false,
-    shop: [],
-    error: null
-}
+  isLoding: false,
+  shop: [],
+  error: null,
+};
 
 export const addShopdata = createAsyncThunk(
+  "category/add",
 
-    'category/add',
+  async (data) => {
+    console.log(data);
 
-    async (data) => {
-        console.log(data);
+    try {
+      const docRef = await addDoc(collection(db, "category"), data);
+      console.log("Document written with ID: ", docRef.id);
 
-        try {
-            const docRef = await addDoc(collection(db, "category"), data);
-            console.log("Document written with ID: ", docRef.id);
-
-            return { ...data, id: docRef.id }
-        } catch (e) {
-            console.error("Error adding document: ", e);
-        }
-
+      return { ...data, id: docRef.id };
+    } catch (e) {
+      console.error("Error adding document: ", e);
     }
-)
+  }
+);
+
+export const deleteShopdata = createAsyncThunk(
+  "category/delete",
+  async (id) => {
+    console.log(id);
+
+    await deleteDoc(doc(db, "category", id));
+
+    return id;
+  }
+);
+
+export const getShopdata = createAsyncThunk("category/get", async () => {
+  let data = [];
+
+  const querySnapshot = await getDoc(collection(db, "category"));
+  querySnapshot.forEach((doc) => {
+    data.push({ ...doc.data(), id: doc.id });
+  });
+
+  return data;
+});
+
+export const updateShopdata = createAsyncThunk("category/put", async (data) => {
+  console.log(data);
+  const washingtonRef = doc(db, "category", data.id);
+
+  let shopData = { ...data, id: data.id };
+  delete shopData.id;
+
+  await updateDoc(washingtonRef, shopData);
+  return data;
+});
 
 export const shopSlice = createSlice({
-    name: 'shop',
-    initialState: initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(addShopdata.fulfilled, (state, action) => {
+  name: "shop",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(addShopdata.fulfilled, (state, action) => {
+      console.log(action.payload);
+      state.isLoding = false;
+      state.shop = state.shop.concat(action.payload);
+      state.error = null;
+    });
 
-            console.log(action.payload);
-            state.isLoding = false;
-            state.shop = state.shop.concat(action.payload);
-            state.error = null;
-        })
-    }
-})
+    builder.addCase(deleteShopdata.fulfilled, (state, action) => {
+      state.shop = state.shop.filter((v) => v.id !== action.payload);
+    });
 
-export default shopSlice.reducer 
+    builder.addCase(getShopdata.fulfilled, (state, action) => {
+      state.shop = action.payload;
+    });
+
+    builder.addCase(updateShopdata.fulfilled, (state, action) => {
+      state.shop = state.shop.map((v) => {
+        if (v.id === action.payload) {
+          return action.payload;
+        } else {
+          return v;
+        }
+      });
+    });
+  },
+});
+
+export default shopSlice.reducer;
