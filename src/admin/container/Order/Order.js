e64a46ff8f649eb17619090e89312afc4a91e345
yatin2123 <yatinv807@gmail.com>
2024-02-22 11:31:45 +0530

    done

--- src/admin/container/Order/Order.js
+++ src/admin/container/Order/Order.js
@@ -17,13 +17,13 @@ function Order(props) {
 
     const [open, setOpen] = React.useState(false);
     const dispatch = useDispatch()
-    
+
     const order = useSelector(state => state.order);
     console.log(order);
 
     useEffect(() => {
         dispatch(getOrder())
-    },[])
+    }, [])
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -44,57 +44,87 @@ function Order(props) {
     });
 
     const columns = [
-        { field: 'id', headerName: 'ID', width: 90,
-        renderCell: (params) => 
-        // console.log(params.row.name);
-        `${params.row.id || ''} `,
-    
-    },
+        {
+            field: 'id', headerName: 'ID', width: 90,
+            // renderCell: (params) =>
+            //     // console.log(params.row.name);
+            //     `${params.row.id || ''} `,
+
+        },
         {
             field: 'firstName',
-            headerName: 'First name',
+            headerName: 'Name',
             width: 150,
             editable: true,
-            renderCell: (params) => 
+            renderCell: (params) => (
+                <>
+                    {params.row.address.map((v, index) => (
+                        <p key={index}>{v.name}</p>
+                    ))}
+                </>
+            ),
                 // console.log(params.row.name);
-                `${params.row.total_amount || ''} `,
-           
+                // `${params.row.address.map((v) => {
+                //     return(
+                //         <p>{v.name}</p>
+                //     )
+                // })} `,
         },
         {
-            field: 'lastName',
-            headerName: 'Last name',
+            field: 'City',
+            headerName: 'City',
             width: 150,
             editable: true,
+            renderCell: (params) => (
+                <>
+                    {params.row.address.map((v, index) => (
+                        <p key={index}>{v.city}</p>
+                    ))}
+                </>
+            ),
         },
         {
-            field: 'age',
-            headerName: 'Age',
+            field: 'Area',
+            headerName: 'Area',
             type: 'number',
             width: 110,
             editable: true,
+            renderCell: (params) => (
+                <>
+                    {params.row.address.map((v, index) => (
+                        <p key={index}>{v.area}</p>
+                    ))}
+                </>
+            ),
         },
         {
-            field: 'fullName',
-            headerName: 'Full name',
-            description: 'This column has a value getter and is not sortable.',
-            sortable: false,
-            width: 160,
-            valueGetter: (params) =>
-                `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+            field: 'State',
+            headerName: 'State',
+            type: 'number',
+            width: 110,
+            editable: true,
+            renderCell: (params) => (
+                <>
+                    {params.row.address.map((v, index) => (
+                        <p key={index}>{v.state}</p>
+                    ))}
+                </>
+            ),
         },
+       
     ];
 
-    // const rows = [
-    //     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-    //     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-    //     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-    //     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-    //     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    //     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    //     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    //     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    //     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    // ];
+    const rows = [
+        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    ];
 
     return (
         <div>
