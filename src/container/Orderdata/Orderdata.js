3ad1350c963a0fcabb241bf250eaceccbf3a74e8
yatin2123 <yatinv807@gmail.com>
2024-03-26 18:00:36 +0530

    done

--- src/container/Orderdata/Orderdata.js
+++ src/container/Orderdata/Orderdata.js
@@ -32,94 +32,91 @@ function Orderdata() {
     return (
         <div >
             <div className='container'>
-
-                {/* <div className='odata'>
-                                        <ul className='ul-data'>
-                                            <li>Order Number </li>
-                                            <li>Order Date</li>
-                                            <li>Order email</li>
-                                            <li>Order total</li>
-                                            <li>Payment methode</li>
-                                        </ul>
-                                       <ul className='ul-data'>
-                                        <li className=''>204</li>
-                                        <li >10/2/2019</li>
-                                        <li >yatin@gmail.com</li>
-                                        <li >10/2/2019</li>
-                                        <li >cash</li>
-                                       </ul>
-                                    </div> */}
-
-                <div className='product-data'>
-                    <h4>Product Details</h4>
-                    {
-                        cartdata.map((v) => {
-                            console.log(v);
-                            const totaldis = v.data.discount * v.data.pro_price / 100;
-                            console.log(totaldis);
-                            if(v.uid === auth.user.uid){
-                                return (
-                                    <>
-    
-                                        <div className='pro-img'>
-                                            <div className='img-data'>
-                                                <img src={v.data.file}></img>
-                                            </div>
-    
-                                            <div className='pro-name'>
-                                                <div>{v.data.pro_name}</div>
-                                            </div>
-                                        </div>
-    
-                                        <div className='row pro-detail'>
-                                            <h5>Product</h5>
-                                            <p>{v.data.pro_price}</p>
-                                        </div>
-                                        <div className='row pro-detail'>
-                                            <h5>Freight</h5>
-                                            <p>Free</p>
-                                        </div>
-                                        <div className='row pro-detail'>
-                                            <h5>Discount</h5>
-                                            <p>{v.data.discount}</p>
-                                        </div>
-                                        <div className='row pro-detail'>
-                                            <h5>Discount Type of Payment:</h5>
-                                            <p>{v.data.discount}</p>
-                                        </div>
-                                        <div className='row pro-detail'>
-                                            <h5>Total Payment:</h5>
-                                            <p>{v.data.discount}</p>
-                                        </div>
-                                        <div className='row pro-detail'>
-                                            <h5>Total :</h5>
-                                            <p>{v.data.pro_price - totaldis}</p>
-                                        </div>
-    
-                                    </>
-                                )
-                            }
-                            
-                        })
-                    }
-
-                </div>
-
-                <div className='user-detail'>
-                    <div className='detail'>
-                        <h4>Delivery address</h4>
-                        <p><spann>Name:</spann> {location.state.fdata.address.map((v) => v.name)}</p>
-                        <p><spann>City:</spann> {location.state.fdata.address.map((v) => v.city)}</p>
-                        <p><spann>Area:</spann> {location.state.fdata.address.map((v) => v.area)}</p>
-                        <p><spann>Email:</spann> {location.state.fdata.address.map((v) => v.email)}</p>
-                        <p><spann>Flat no. :</spann> {location.state.fdata.address.map((v) => v.flat_no)}</p>
-                        <p><spann>Pincode:</spann> {location.state.fdata.address.map((v) => v.pincode)}</p>
-                        <p><spann>Landmark:</spann> {location.state.fdata.address.map((v) => v.landmark)}</p>
+                <div class="container">
+                    <div class="title">
+                        <h2>Product Order Form</h2>
                     </div>
-                    <div className='notifiction'>
-                        <h4>SMS Notifiction</h4>
-                        <h6>Celphone registered: <br></br> (00) 215-DUNSK</h6>
-                        <input placeholder='enter name'></input>
+                    <div class="d-flex">
+                        <form action="" method="">
+                            {
+                                cartdata.map((v) => {
+                                    console.log(v);
+                                    const totaldis = v.data.discount * v.data.pro_price / 100;
+                                    console.log(totaldis);
+                                    if (v.uid === auth.user.uid) {
+                                        return (
+                                            <>
+                                                <label>
+                                                    <span>Product Image <span class="required">*</span></span>
+                                                    <img src={v.data.file}></img>
+                                                </label>
+
+                                                <label>
+                                                    <span>Product Price<span class="required">*</span></span>
+                                                    <h6>{v.data.pro_price}</h6>
+                                                </label>
+                                                <label>
+                                                    <span>Freight<span class="required">*</span></span>
+
+                                                    <h6>Free</h6>
+                                                </label>
+                                                <label>
+                                                    <span>Discount <span class="required">*</span></span>
+
+                                                    <h6>{v.data.discount}</h6>
+                                                </label>
+                                                <label>
+                                                    <span>Discount Type of Payment: <span class="required">*</span></span>
+
+                                                    <h6>{v.data.discount}</h6>
+                                                </label>
+                                                <label>
+                                                    <span>Total Payment: <span class="required">*</span></span>
+
+                                                    <h6>{v.data.discount}</h6>
+                                                </label>
+                                                <label>
+                                                    <span>Total : <span class="required">*</span></span>
+
+                                                    <h6>{v.data.pro_price - totaldis}</h6>
+                                                </label>
+                                            </>
+                                        )
+                                    }
+                                })
+                            }
+
+
+                            <div className='detail'>
+                                <h4>Delivery address</h4>
+                                <p><spann>Name:</spann> {location.state.fdata.address.map((v) => v.name)}</p>
+                                <p><spann>City:</spann> {location.state.fdata.address.map((v) => v.city)}</p>
+                                <p><spann>Area:</spann> {location.state.fdata.address.map((v) => v.area)}</p>
+                                <p><spann>Email:</spann> {location.state.fdata.address.map((v) => v.email)}</p>
+                                <p><spann>Flat no. :</spann> {location.state.fdata.address.map((v) => v.flat_no)}</p>
+                                <p><spann>Pincode:</spann> {location.state.fdata.address.map((v) => v.pincode)}</p>
+                                <p><spann>Landmark:</spann> {location.state.fdata.address.map((v) => v.landmark)}</p>
+                            </div>
+                        </form>
+                        <div class="Yorder">
+                            <table>
+                                <tr>
+                                    <th colspan="2">Your order</th>
+                                </tr>
+
+                                <tr>
+                                    <td>Shipping</td>
+                                    <td>{location.state.fdata.status}</td>
+                                </tr>
+                            </table><br />
+                            <div>
+                                <input type="radio" name="dbt" value="dbt" checked /> Direct Bank Transfer
+                            </div>
+                            <p>
+                                Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order will not be shipped until the funds have cleared in our account.
+                            </p>
+
+                        </div>
                     </div>
                 </div>
 
